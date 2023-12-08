3d68951d72b8826495d460fb74a528b23dfcda02
pepelindr13 <codybrownie101@gmail.com>
2023-12-08 20:45:50 +0100

    scroll

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function calc() {
+  window.scrollTo(0, 4);
   let expense = Number(phone.value) + Number(food.value) + Number(coffee.value) + Number(health.value) + Number(trans.value) + Number(oth.value)
   if (expense > 10000) {
     err.innerHTML = "You can't spend more than #10,000 in a day!"
   }else if (phone.value == '' || food.value == '' || coffee.value == '' || health.value == '' || trans.value == '' || oth.value == '') {
     err.innerHTML = "Fill in all fields!"
+    // exp.innerHTML = "-#0"
   }else {
     exp.innerHTML = "-#" + expense
     bal.innerHTML = "#" + (10000 - expense)
+    err.innerHTML = ""
   }
 
   phone.value = ''
@@ -15,6 +18,5 @@ function calc() {
   health.value = ''
   oth.value = ''
   trans.value = ''
-
 
 }
